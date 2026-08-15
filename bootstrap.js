53377b2890a49e21c3fd37eff1626cc8f1adc7da
fmspurlog-blip <fms.purlog@gmail.com>
2026-08-15 21:34:18 +0800

    Bump dashboard UI cache to 74.5

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
 (async()=>{
-  const q='74.4';
+  const q='74.5';
   const addCss=(href)=>{const l=document.createElement('link');l.rel='stylesheet';l.href=href;document.head.appendChild(l)};
   addCss('v6.css?v='+q);
   addCss('v7.css?v='+q);
